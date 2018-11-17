71c46fa7c0e871573b26b31335028c82eb62124c
moonygo <mhj1667@gmail.com>
2018-11-17 14:07:37 +0900

    data and model upload
    stan, r script, data file upload

---
+++ Ex1_EightSchools.R
@@ -0,0 +1,15 @@
+schools_dat <- list(J = 8, 
+                    y = c(28,  8, -3,  7, -1,  1, 18, 12),
+                    sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
+fit <- stan(file = '~/STEM_TechSquare2018/stan_programs/8schools.stan', data = schools_dat)
+
+print(fit)
+plot(fit)
+pairs(fit, pars = c("mu", "tau", "lp__"))
+
+la <- extract(fit, permuted = TRUE) # return a list of arrays 
+mu <- la$mu 
+
+### return an array of three dimensions: iterations, chains, parameters 
+a <- extract(fit, permuted = FALSE) 
+
---
+++ Ex2_Rats.R
@@ -0,0 +1,6 @@
+y <- as.matrix(read.table('https://raw.github.com/wiki/stan-dev/rstan/rats.txt', header = TRUE))
+x <- c(8, 15, 22, 29, 36)
+xbar <- mean(x)
+N <- nrow(y)
+T <- ncol(y)
+rats_fit <- stan('https://raw.githubusercontent.com/stan-dev/example-models/master/bugs_examples/vol1/rats/rats.stan')
